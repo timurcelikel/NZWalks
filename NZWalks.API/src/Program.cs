8ca92f8b3295753e409448b2ca1cd27406cd11dc
Timur Celikel <timur.celikel@proton.me>
2024-03-13 13:25:00 -0700

    initial project commit

---
+++ NZWalks.API/src/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace nzwalks.api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((builderContext, config) => { config.AddEnvironmentVariables(); })
+                .Build();
+    }
+}
